58e102971aac223c21315ec57fbaae1860111dcd
TomChiu <iiooiio@hotmail.com>
2021-01-27 09:10:45 +0800

    update -> 流數據與無界資料測試(from netcat)

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # flink
-## with scala
+## with java
+
+####測試無界數據
+
+#####windows: 
+1. 下載netcat
+2. 配置path
+3. cmd下執行  nc -l -p 7777
+4. 啟動StreamWordCount.java 監聽7777端口
+5. 往內輸入字串
+6. 可見到類似以下的輸出
+
+* 8> (456,1)
+* 2> (123,1)
+* 7> (789,1)
+* 2> (i,1)
+* 4> (your,1)
+* 2> (am,1)
+* 2> (father,1)
+* 2> (123,2)
+
+
---
+++ src/main/java/com/example/wc/StreamWordCount.java
@@ -0,0 +1,46 @@
+package com.example.wc;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * @author User
+ */
+public class StreamWordCount {
+    public static void main(String[] args) throws  Exception
+    {
+
+        //  流數據架構: 等數據來再一個一個輸出
+
+        // 創建流處理執行環境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 設定併行度(不設定的話默認是4 ,因為取決於電腦核心數量)
+        env.setParallelism(8);
+
+        //env.setParallelism(1); 也可以設為1 這樣的話只有一條thread
+
+        // 從文件中讀取數據(有界流 開發環境測試用)
+        //String inputPath = "D:\\flink\\src\\main\\resources\\hello.txt";
+        //DataStream<String> inputDataStream = env.readTextFile(inputPath);
+
+
+        // 真正意義上的流式數據
+        // 從socket文本流 讀取數據
+        DataStream<String> inputDataStream = env.socketTextStream("localhost", 7777);
+
+        // 基於數據流 進行轉換計算
+        SingleOutputStreamOperator<Tuple2<String, Integer>> resultStream = inputDataStream.flatMap(new WordCount.MyFlatMapper())
+                .keyBy(0)
+                .sum(1);
+        resultStream.print();//此處並沒有執行任務
+
+        // 啟動任務
+        env.execute();
+    }
+}
